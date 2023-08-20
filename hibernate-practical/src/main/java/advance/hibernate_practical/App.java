e0b0bda67f86c9de0a1976328169e659bd726c86
Junaid <programming4hacking@gmail.com>
2023-08-20 15:37:05 +0500

    Configured Hibernate Settings

--- hibernate-practical/src/main/java/advance/hibernate_practical/App.java
+++ hibernate-practical/src/main/java/advance/hibernate_practical/App.java
@@ -1,9 +1,20 @@
 package advance.hibernate_practical;
 
-public class App 
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println( "Hello World!" );
+    	System.out.println("Hello");
+    	SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+    	System.out.println(factory);
+
+        System.out.println(factory.isClosed());
+
     }
 }
+
+
